2b1815811ff8074c4a51f931fe1b774c1c735423
martinst1 <martinst@gmail.com>
2017-03-13 17:07:19 +0200

    Extracted original dbcontext, user, connection strings changed

---
+++ Brandviser/Brandviser.Data/BrandviserDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brandviser.Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Brandviser.Data
+{
+    public class BrandviserDbContext : IdentityDbContext<User>
+    {
+        public BrandviserDbContext()
+            : base("BrandviserDb", throwIfV1Schema: false)
+        {
+        }
+
+        public static BrandviserDbContext Create()
+        {
+            return new BrandviserDbContext();
+        }
+    }
+}
--- Brandviser/Brandviser.Web/Controllers/AccountController.cs
+++ Brandviser/Brandviser.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Brandviser.Web.Models;
+using Brandviser.Data.Models;
 
 namespace Brandviser.Web.Controllers
 {
@@ -151,7 +152,7 @@ namespace Brandviser.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -367,7 +368,7 @@ namespace Brandviser.Web.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
