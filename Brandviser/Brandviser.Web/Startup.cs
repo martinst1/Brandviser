00a358385a7cf071c1e110a1d33ced83e4396d4e
martinst1 <martinst@gmail.com>
2017-03-13 13:40:30 +0200

    Initial

---
+++ Brandviser/Brandviser.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Brandviser.Web.Startup))]
+namespace Brandviser.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
