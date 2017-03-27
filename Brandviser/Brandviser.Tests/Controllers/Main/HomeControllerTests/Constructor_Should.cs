16c5c87f0af2adce5c16906bab0b1ad01a20b8b7
martinst1 <martinst@gmail.com>
2017-03-27 18:58:29 +0300

    Adding homecontroller tests

---
+++ Brandviser/Brandviser.Tests/Controllers/Main/HomeControllerTests/Constructor_Should.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brandviser.Services.Contracts;
+using Brandviser.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace Brandviser.Tests.Controllers.Main.HomeControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void NotThrow_WhenCorrectParamsArePassed()
+        {
+            // Arrange
+            var domainService = new Mock<IDomainService>();
+
+
+            // Act and Assert
+            Assert.DoesNotThrow(() => new HomeController(domainService.Object));
+        }
+
+        [Test]
+        public void Create_Instance_Of_Controller_WhenArgumentsAreCorrect()
+        {
+            // Arrange
+            var domainService = new Mock<IDomainService>();
+
+
+            // Act
+            var homeController = new HomeController(domainService.Object);
+
+            // Assert
+            Assert.IsInstanceOf<HomeController>(homeController);
+        }
+
+        [Test]
+        public void ThrowArgumentNullExceptionWithMessageContaining_IDomainService_WhenDomainServiceIsNull()
+        {
+          
+            // Act and Assert
+            Assert.That(() =>
+            new HomeController(null),
+            Throws.InstanceOf<ArgumentNullException>().With.Message.Contains("IDomainService"));
+        }
+    }
+}
---
+++ Brandviser/Brandviser.Tests/Controllers/Main/HomeControllerTests/Index_Should.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brandviser.Data.Models;
+using Brandviser.Services.Contracts;
+using Brandviser.Web.Controllers;
+using Brandviser.Web.Models;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace Brandviser.Tests.Controllers.Main.HomeControllerTests
+{
+    [TestFixture]
+    public class Index_Should
+    {
+        [Test]
+        public void RenderDefaultView_WithExpectedViewModel()
+        {
+            // Arrange
+            var domainService = new Mock<IDomainService>();
+            var homeController = new HomeController(domainService.Object);
+
+            var domains = new List<Domain>()
+            {
+                new Domain() { Name = "name", LogoUrl = "logourl", OriginalOwnerCustomPrice = 1, Id = 1 }
+            };
+
+            domainService.Setup(d => d.GetLatestEightPublishedDomains()).Returns(domains.AsQueryable<Domain>);
+
+            // Act & Assert
+
+            homeController
+                .WithCallTo(h => h.Index())
+                .ShouldRenderDefaultView()
+                .WithModel<SearchViewModel>(
+                s =>
+                {
+                    Assert.AreEqual(1, s.Domains.Count());
+                    Assert.AreEqual("name", s.Domains.First().Name);
+                    Assert.AreEqual("logourl", s.Domains.First().LogoUrl);
+                    Assert.AreEqual(1, s.Domains.First().Price);
+                    Assert.AreEqual(1, s.Domains.First().Id);
+                });
+        }
+
+        [Test]
+        public void Call_GetLatestEightPublishedDomains_Of_DomainService_Once()
+        {
+            // Arrange
+            var domainService = new Mock<IDomainService>();
+            var homeController = new HomeController(domainService.Object);
+
+            // Act
+            homeController.Index();
+
+            // Assert
+            domainService.Verify(d => d.GetLatestEightPublishedDomains(), Times.Once());
+        }
+    }
+}
