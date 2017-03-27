6cf22c40c7063c9087abd140fe54f8c8b0677384
martinst1 <martinst@gmail.com>
2017-03-27 16:55:16 +0300

    Finished user service tests

---
+++ Brandviser/Brandviser.Tests/Services/UserServiceTests/TopUpUserBalance_Should.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brandviser.Common.Contracts;
+using Brandviser.Data.Contracts;
+using Brandviser.Data.Models;
+using Brandviser.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace Brandviser.Tests.Services.UserServiceTests
+{
+    [TestFixture]
+    public class TopUserBalance_Should
+    {
+        [Test]
+        public void Call_GetByStringId_Of_UserRepository_Once()
+        {
+            // Arrange
+            var brandviserData = new Mock<IBrandviserData>();
+            var userRepository = new Mock<IEfRepository<User>>();
+            var dateTimeProvider = new Mock<IDateTimeProvider>();
+            var userId = "userId";
+
+            var user = new User() { Id = userId };
+            var amount = 1m;
+
+            var userService = new UserService(brandviserData.Object, dateTimeProvider.Object);
+
+            userRepository.Setup(r => r.GetByStringId(userId))
+                .Returns(user);
+
+            brandviserData.Setup(b => b.Users).Returns(userRepository.Object);
+
+
+            // Act
+            userService.TopUpUserBalance(userId, amount);
+
+            // Assert
+            userRepository.Verify(u => u.GetByStringId(userId), Times.Once());
+        }
+
+        [Test]
+        public void Set_Data_Correctly()
+        {
+            // Arrange
+            var brandviserData = new Mock<IBrandviserData>();
+            var userRepository = new Mock<IEfRepository<User>>();
+            var dateTimeProvider = new Mock<IDateTimeProvider>();
+            var userId = "userId";
+
+            var user = new User() { Id = userId };
+            var amount = 1m;
+
+            var userService = new UserService(brandviserData.Object, dateTimeProvider.Object);
+
+            userRepository.Setup(r => r.GetByStringId(userId))
+                .Returns(user);
+
+            brandviserData.Setup(b => b.Users).Returns(userRepository.Object);
+
+            // Act
+            userService.TopUpUserBalance(userId, amount);
+
+            // Assert
+            Assert.AreEqual(1, user.Balance);
+        }
+
+        [Test]
+        public void Call_SaveChanges_Of_BrandviserData_Once()
+        {
+            // Arrange
+            var brandviserData = new Mock<IBrandviserData>();
+            var userRepository = new Mock<IEfRepository<User>>();
+            var dateTimeProvider = new Mock<IDateTimeProvider>();
+            var userId = "userId";
+
+            var user = new User() { Id = userId };
+            var amount = 1m;
+
+            var userService = new UserService(brandviserData.Object, dateTimeProvider.Object);
+
+            userRepository.Setup(r => r.GetByStringId(userId))
+                .Returns(user);
+
+            brandviserData.Setup(b => b.Users).Returns(userRepository.Object);
+
+
+            // Act
+            userService.TopUpUserBalance(userId, amount);
+
+            // Assert
+            brandviserData.Verify(u => u.SaveChanges(), Times.Once());
+        }
+    }
+}
---
+++ Brandviser/Brandviser.Tests/Services/UserServiceTests/TransferAmountFromBuyerToSeller_Should.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brandviser.Common.Contracts;
+using Brandviser.Data.Contracts;
+using Brandviser.Data.Models;
+using Brandviser.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace Brandviser.Tests.Services.UserServiceTests
+{
+    [TestFixture]
+    public class TransferAmountFromBuyerToSeller_Should
+    {
+        [Test]
+        public void Call_GetByStringId_Of_UserRepository_Twice()
+        {
+            // Arrange
+            var brandviserData = new Mock<IBrandviserData>();
+            var userRepository = new Mock<IEfRepository<User>>();
+            var dateTimeProvider = new Mock<IDateTimeProvider>();
+            var buyerId = "buyerId";
+            var sellerId = "sellerId";
+
+            var buyer = new User() { Id = buyerId };
+            var seller = new User() { Id = sellerId };
+            var amount = 1m;
+
+            var userService = new UserService(brandviserData.Object, dateTimeProvider.Object);
+
+            userRepository.Setup(r => r.GetByStringId(buyerId))
+                .Returns(buyer);
+
+            userRepository.Setup(r => r.GetByStringId(sellerId))
+               .Returns(seller);
+
+            brandviserData.Setup(b => b.Users).Returns(userRepository.Object);
+
+
+            // Act
+            userService.TransferAmountFromBuyerToSeller(buyerId, amount, sellerId);
+
+            // Assert
+            userRepository.Verify(u => u.GetByStringId(buyerId), Times.Once());
+            userRepository.Verify(u => u.GetByStringId(sellerId), Times.Once());
+        }
+
+        [Test]
+        public void Set_Data_Correctly()
+        {
+            // Arrange
+            var brandviserData = new Mock<IBrandviserData>();
+            var userRepository = new Mock<IEfRepository<User>>();
+            var dateTimeProvider = new Mock<IDateTimeProvider>();
+            var buyerId = "buyerId";
+            var sellerId = "sellerId";
+
+            var buyer = new User() { Id = buyerId };
+            var seller = new User() { Id = sellerId };
+            var amount = 1m;
+
+            var userService = new UserService(brandviserData.Object, dateTimeProvider.Object);
+
+            userRepository.Setup(r => r.GetByStringId(buyerId))
+                .Returns(buyer);
+
+            userRepository.Setup(r => r.GetByStringId(sellerId))
+               .Returns(seller);
+
+            brandviserData.Setup(b => b.Users).Returns(userRepository.Object);
+
+            
+            // Act
+            userService.TransferAmountFromBuyerToSeller(buyerId, amount, sellerId);
+
+            // Assert
+            Assert.AreEqual(-1, buyer.Balance);
+            Assert.AreEqual(1, seller.Balance);
+        }
+
+        [Test]
+        public void Call_SaveChanges_Of_BrandviserData_Twice()
+        {
+            // Arrange
+            var brandviserData = new Mock<IBrandviserData>();
+            var userRepository = new Mock<IEfRepository<User>>();
+            var dateTimeProvider = new Mock<IDateTimeProvider>();
+            var buyerId = "buyerId";
+            var sellerId = "sellerId";
+
+            var buyer = new User() { Id = buyerId };
+            var seller = new User() { Id = sellerId };
+            var amount = 1m;
+
+            var userService = new UserService(brandviserData.Object, dateTimeProvider.Object);
+
+            userRepository.Setup(r => r.GetByStringId(buyerId))
+                .Returns(buyer);
+
+            userRepository.Setup(r => r.GetByStringId(sellerId))
+               .Returns(seller);
+
+            brandviserData.Setup(b => b.Users).Returns(userRepository.Object);
+
+
+            // Act
+            userService.TransferAmountFromBuyerToSeller(buyerId, amount, sellerId);
+
+            // Assert
+            brandviserData.Verify(b => b.SaveChanges(), Times.Exactly(2));
+        }
+    }
+}
